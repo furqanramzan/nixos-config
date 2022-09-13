2b065322ee60dd53f2a7c290e88a29ed3e482933
Muhammad Furqan <furqanramzan271996@gmail.com>
2022-09-13 16:14:49 +0500

    feat: add ark (GUI bases file compression utility)

--- system/programs/plasma.nix
+++ system/programs/plasma.nix
@@ -1,6 +1,8 @@
-{
+{pkgs, ...}: {
   # Enable plasma desktop manager.
   services.xserver = {
     desktopManager.plasma5.enable = true;
   };
+
+  environment.systemPackages = [pkgs.libsForQt5.ark];
 }
