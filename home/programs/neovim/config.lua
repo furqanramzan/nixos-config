0fa0b29ae83cdaa7c14978428bba584ca4b94ceb
Muhammad Furqan <furqanramzan271996@gmail.com>
2022-09-13 15:41:31 +0500

    feat(neovim): format on save

--- home/programs/neovim/config.lua
+++ home/programs/neovim/config.lua
@@ -17,3 +17,7 @@ vim.opt.signcolumn = "yes"
 
 vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
 vim.keymap.set('n', '<leader>qq', '<cmd>q<cr>')
+
+-- Format on save
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
+
