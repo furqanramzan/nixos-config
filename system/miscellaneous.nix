245b5e189894069839abbeea1ea509d7af0618b6
Muhammad Furqan <muhammad.furqan@cooperativecomputing.com>
2022-10-13 19:26:54 +0500

    feat: enable openssl

--- system/miscellaneous.nix
+++ system/miscellaneous.nix
@@ -37,6 +37,9 @@
     };
   };
 
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
