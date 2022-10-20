20609b153bd79abb7cd8477d9e7512e1a7a7814f
Muhammad Furqan <muhammad.furqan@cooperativecomputing.com>
2022-10-20 20:35:57 +0500

    feat(neovim): Update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file.

--- home/programs/neovim/tree.nix
+++ home/programs/neovim/tree.nix
@@ -5,7 +5,11 @@
       plugin = nvim-tree-lua;
       type = "lua";
       config = ''
-        require('nvim-tree').setup{}
+        require('nvim-tree').setup({
+          update_focused_file = {
+            enable = true,
+          },
+        })
         vim.keymap.set('n', '<leader>ee', '<cmd>NvimTreeToggle<cr>')
       '';
     }
