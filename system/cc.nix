2ddc096f3f43c0d577209fef68f462fc5e6d3eea
Muhammad Furqan <furqanramzan271996@gmail.com>
2022-10-30 16:02:02 +0500

    feat(config): update pc and cc configs

--- system/cc.nix
+++ system/cc.nix
@@ -4,7 +4,5 @@
   ];
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
-  boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
 }
--- system/pc.nix
+++ system/pc.nix
@@ -3,8 +3,5 @@
     ./default.nix
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
+  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod"];
 }
