ae879f30033979fa6cb8d31bfdaac1b0950a7566
Muhammad Furqan <muhammad.furqan@cooperativecomputing.com>
2022-12-22 14:31:28 +0500

    feat(fzf): add more configs

--- home/programs/cli/fzf.nix
+++ home/programs/cli/fzf.nix
@@ -1,7 +1,32 @@
 {
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (config.colorscheme) colors;
+in {
   programs.fzf = {
     enable = true;
     enableFishIntegration = true;
     enableBashIntegration = true;
+    changeDirWidgetOptions = [
+      "--preview 'tree -C {} | head -200'"
+    ];
+    defaultOptions = [
+      "--height 80%"
+      "--border"
+      "--reverse"
+    ];
+    fileWidgetOptions = [
+      "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
+    ];
+    historyWidgetOptions = [
+      "--sort"
+      "--exact"
+    ];
+    tmux = {
+      enableShellIntegration = true;
+    };
   };
+  home.packages = with pkgs; [tree];
 }
